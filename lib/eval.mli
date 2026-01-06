dbc5789b935496dd95076ed1c6f0330bdc22fba6
Christian Lindig <lindig@gmail.com>
2026-01-06 20:39:07 +0000

    Document Eval interface
    Signed-off-by: Christian Lindig <lindig@gmail.com>

--- lib/eval.mli
+++ lib/eval.mli
@@ -2,16 +2,39 @@
  * SPDX-License-Identifier: Unlicense
  *)
 exception Failure of string
+(** Any problem is reported: syntax of an expression or unknown bindings *)
 
-type expression
 type value = Bool of bool | Float of float | String of string
+type expression
+(* See the README and parser.mly for the supported expression syntax *)
+
 type env
+(** An [env] binds a names to [value]s. *)
 
 val empty : unit -> env
+(** [env] with no names *)
+
 val add : env -> string -> value -> unit
+(** Add a new binding to [env] *)
+
 val add' : env -> string list -> value -> unit
+(** Add multiple bindings, all referring to the same [value]. Hence this creates
+    aliases *)
+
 val env : (string * value) list -> env
+(** Create an [env] from an association list *)
+
 val compile : string -> expression
+(** Compile a string to an expression for later evaluation *)
+
 val string : env -> string -> value
+(** Evaluate a string given an expression and evaluate it. This uses [compile]
+    internally and is most useful if the expression is only evaluated once *)
+
 val expr : env -> expression -> value
+(** Evaluate a (compiled) expression. This can be used to evaluate a single
+    expression multiple times with different [env] environments *)
+
 val simple : string -> value
+(** This simplest case: evaluate a string expression with an empty environment
+    to a value *)
